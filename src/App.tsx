524b076229224844372e7735b423dc25180654bd
gregolly <gregollyff@hotmail.com>
2022-07-04 15:23:52 -0300

    Conceitos sobre context api

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Modal from "react-modal";
+import { TransactionsProvider } from "./hooks/useTransactions";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from "./components/NewTransactionModal";
@@ -20,16 +21,19 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsProvider>
         <Header 
           onOpenNewTransactionModal={handleOpenNewTransactionModal} 
         />
+
         <Dashboard />
+
         <NewTransactionModal 
           isOpen={isNewTransactionModalOpen}
           onRequestClose={handleCloseNewTransactionModal} 
         />
+
         <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
